d52c2406f6d4b2c49020ca3391cac0db7667bf81
Xi Chen <putcncx@gmail.com>
2018-07-11 11:36:31 -0700

    add lidar document

--- lidar_lite/LidarLite.py
+++ lidar_lite/LidarLite.py
@@ -4,6 +4,41 @@ import threading
 
 from usb_port_finder import wait_until_arduino_connected
 
+'''
+
+How is it connected
+
+LIDAR-Lite v3HP (https://www.sparkfun.com/products/14599) is going to be 
+connected via i2c to an Arduino, which is connected to laptop via USB, and 
+control system will talk to the Arduino via serial.
+
+Why did we add extra Arduino
+
+LIDAR-Lite v3HP provides 2 date interfaces, i2c or PWM. Currently Phidget does
+not have a i2c adaptor, we tried to go the PWM way, but "Versatile Input" is not
+fast enough to work with PWM mearsuring; On the other hand, LIDAR-Lite v3HP
+also provides a Arduino library to be easy to work with, that's why we go with
+extra Arduino in our system
+
+Wiring Guide
+
+Mostly followed https://learn.sparkfun.com/tutorials/lidar-lite-v3-hookup-guide, 
+but we skipped getting 5v from Arduino, but from our powerful 5VDC source 
+instead, and we do not need the 1000μF capacitor either. the wiring is as:
+
+Lidar Wire        To
+Red               Power source 5V
+Black             Power source GND And Ardino Power GND
+Blue              Arduino SDA
+Green             Arduino SCL
+Yellow            Leave open
+Orange            Leave open
+
+Arduino Code
+https://learn.sparkfun.com/tutorials/lidar-lite-v3-hookup-guide
+
+'''
+
 
 class LidarLite(object):
     def __init__(self):
